9e14b1105fae1a447450967c78898bda781b879b
BobbyWasabiReal <wasabibob13@gmail.com>
2022-10-18 15:59:10 -0700

    Fixed the home page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/skills');
+var skillsRouter = require('./routes/skills');
 
 var app = express();
 
